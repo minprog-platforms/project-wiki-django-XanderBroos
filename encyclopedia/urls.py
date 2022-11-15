4d4ec6a80ca765c561442fb1f816218021c4fc0e
XanderBroos <broosxander@gmail.com>
2022-11-15 10:39:04 +0100

    day 1 entry page and layout create

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("create", views.create, name="create"), 
+    path("wiki/<str:name>", views.wiki, name="wiki"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,38 @@
 from django.shortcuts import render
+# from django.http import HttpResponse
 
 from . import util
+import markdown2
+from django import forms
+
+def create(request):
+    return render(request, "encyclopedia/create.html", {
+        # "save": util.save_entry(name, "name.md")
+        "name": util.list_entries()
+    })
+    
+def wiki(request, name): 
+    if util.get_entry(name) == None:
+        return render(request, "encyclopedia/entry.html", {
+            # "name": util.list_entries()
+            "title": "Not found", "body": "Not Available, try creating it"
+        })
+    f = open(f"entries/{name}.md","r")
+    f = f.read()
+    html = markdown2.markdown(f)
+    
+    return render(request, "encyclopedia/entry.html", {
+        # "name": util.list_entries()
+
+        "title": name, "body2": html
+    })
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-
+    
+    
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
